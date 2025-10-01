ba3907875f54382cdfde7ea5d2fff763903d4f0d
mzinpegex <moisesarsan@gmail.com>
2025-10-01 14:29:42 -0300

    new

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 import axios from "axios";
 import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
 
 dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors({origin: process.env.TRUSTED_FRONTEND}))
+
 const API_KEY = process.env.API_KEY;
 
-async function Request(params) {
+app.get("/assets", async(req, res) => {
     try {
-        const response = axios.get("https://apis.roblox.com/oauth/v1/credentials", {
+        const response = await axios.get(`https://apis.roblox.com/oauth/v1/credentials`, {
             headers: {
                 "x-api-key": API_KEY,
             },
         });
-        
-        console.log((await response).data);
-    } catch (err) {
-        console.error("Erro na request:", err.response?.status, err.response?.data || err.message);
+    } catch(err) {
+        res.status(500).json({error: err.message})
     }
-}
+});
 
-Request();
+app.listen(3000, () => console.log("porta 3000"))
